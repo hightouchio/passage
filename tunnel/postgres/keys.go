f5ab11f2e5daba1a2f3f3793dd98044320c75e40
Andrew Page <andrew@andrewpage.me>
2021-05-11 18:37:31 -0700

    normal tunnel private key authentication based on keys table

--- tunnel/postgres/keys.go
+++ tunnel/postgres/keys.go
@@ -6,31 +6,64 @@ import (
 	"github.com/pkg/errors"
 )
 
-func (c Client) GetReverseTunnelAuthorizedKeys(ctx context.Context, tunnelID int) ([]string, error) {
-	rows, err := c.db.QueryContext(ctx, getReverseTunnelAuthorizedKeys, tunnelID)
+type Key struct {
+	ID       int
+	Type     string
+	Contents string
+}
+
+func (c Client) GetNormalTunnelPrivateKeys(ctx context.Context, tunnelID int) ([]Key, error) {
+	return c.getKeysForTunnel(ctx, getNormalTunnelPrivateKeys, tunnelID)
+}
+
+func (c Client) GetReverseTunnelAuthorizedKeys(ctx context.Context, tunnelID int) ([]Key, error) {
+	return c.getKeysForTunnel(ctx, getReverseTunnelAuthorizedKeys, tunnelID)
+}
+
+func (c Client) getKeysForTunnel(ctx context.Context, query string, tunnelID int) ([]Key, error) {
+	rows, err := c.db.QueryContext(ctx, query, tunnelID)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return []string{}, nil
+			return []Key{}, nil
 		}
 
-		return []string{}, err
+		return []Key{}, err
 	}
 
-	results := make([]string, 0)
+	results := make([]Key, 0)
 	for rows.Next() {
-		var row string
-		if err := rows.Scan(&row); err != nil {
-			return []string{}, errors.Wrap(err, "could not scan")
+		key, err := scanKey(rows)
+		if err != nil {
+			return []Key{}, errors.Wrap(err, "could not scan")
 		}
-		results = append(results, row)
+		results = append(results, key)
 	}
 
 	return results, nil
 }
 
+func scanKey(scan scanner) (Key, error) {
+	var key Key
+	if err := scan.Scan(
+		&key.ID,
+		&key.Type,
+		&key.Contents,
+	); err != nil {
+		return Key{}, err
+	}
+	return key, nil
+}
+
+const getNormalTunnelPrivateKeys = `
+SELECT passage.keys.id, passage.keys.key_type, passage.keys.contents FROM passage.keys
+JOIN passage.key_authorizations ON passage.keys.id=passage.key_authorizations.key_id
+JOIN passage.tunnels ON passage.key_authorizations.tunnel_id=passage.tunnels.id AND passage.key_authorizations.tunnel_type='normal'
+WHERE passage.keys.key_type='private' AND passage.tunnels.id=$1;
+`
+
 const getReverseTunnelAuthorizedKeys = `
-SELECT passage.keys.contents FROM passage.keys
-JOIN passage.key_authorizations ON passage.keys.id=passage.key_authorizations.key_id 
+SELECT passage.keys.id, passage.keys.key_type, passage.keys.contents FROM passage.keys
+JOIN passage.key_authorizations ON passage.keys.id=passage.key_authorizations.key_id
 JOIN passage.reverse_tunnels ON passage.key_authorizations.tunnel_id=passage.reverse_tunnels.id AND passage.key_authorizations.tunnel_type='reverse'
 WHERE passage.keys.key_type='public' AND passage.reverse_tunnels.id=$1;
 `
