a49d11b13131242222b53133052afaa6f233cbcc
Andrew Page <andrew@andrewpage.me>
2021-10-21 17:58:09 -0700

    remove deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-image_id=$1
-service=$2
-
-target_tag=target.production
-
-docker pull 810028259040.dkr.ecr.us-east-1.amazonaws.com/passage:$image_id
-docker tag 810028259040.dkr.ecr.us-east-1.amazonaws.com/passage:$image_id 810028259040.dkr.ecr.us-east-1.amazonaws.com/passage:$target_tag
-docker push 810028259040.dkr.ecr.us-east-1.amazonaws.com/passage:$target_tag
-aws ecs update-service --cluster passage_$service --service passage_$service --force-new-deployment
