4f08060282ed1771c3ca246bd740ca1a62c373cb
Andrew Page <andrew@andrewpage.me>
2021-12-23 18:56:17 -0800

    cleanup e2e

--- test/test.sh
+++ test/test.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 
+set -e
+
+# Teardown environment.
+cleanup() {
+  docker-compose -f test/docker-compose.yml down --volumes
+  exit $?
+}
+trap cleanup EXIT
+
 # Spin up environment.
 docker-compose -f test/docker-compose.yml build passage
 docker-compose -f test/docker-compose.yml up -d
 
 # Execute tests.
 ./test/runner.sh
-
-# Teardown environment.
-docker-compose -f test/docker-compose.yml down --volumes
